ca6f6455bf6b3fe25d1a42062496f589b77101a3
Gustavo-7423 <gustavoantonio2310@gmail.com>
2024-11-05 12:07:41 -0300

    Update control_3.py

--- control_3.py
+++ control_3.py
@@ -1,20 +1,7 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+
 
 datos = pd.read_csv("Datos_históricos_del_S&P_500.csv")
 print(datos)
 
-"""st.sidebar.title("Esta es una prueba")
-st.sidebar.header("Hola esto es una barra lateral")
-st.sidebar.write("Barra lateral ")
-
-st.sidebar.image("madrir.png")
-
-if st.sidebar.button("Clik en la barra lateral "):
-    st.sidebar.write("Hice un boton lateral ")
-user_input = st.sidebar.text_input("escribe algo en la barra ") # esto hace que el usuario ingrese palabra y aparezcan en la barra lateral 
-st.sidebar.write("Escribiste en la barra:  ", user_input)
-st.title("Esta es mi primera pagina")
-st.header("MI primera página")
-st.image("madrir.png")"""
