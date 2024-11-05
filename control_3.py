a4bd651afb5e1f3c3a058e679b7b3c34726b474b
Gustavo-7423 <gustavoantonio2310@gmail.com>
2024-11-05 12:14:19 -0300

    Update control_3.py

--- control_3.py
+++ control_3.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 
 
-st.markdown("# datos historicos del indice bursatil S&P_500")
+st.markdown("# Datos historicos del indice bursatil S&P_500") 
 
 datos = pd.read_csv("Datos_históricos_del_S&P_500.csv")
 print(datos)
-st.area_chart(datos)
+st.bar_chart(datos)
