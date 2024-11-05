c8a531f1bfcc79f4d74d0c7d545dfc6a80cf9842
Gustavo-7423 <gustavoantonio2310@gmail.com>
2024-11-05 15:49:28 -0300

    Update control_3.py

--- control_3.py
+++ control_3.py
@@ -4,8 +4,8 @@ import pandas as pd
 
 st.markdown("# Datos historicos del indice bursatil S&P_500") 
 st.markdown("<p style='font-size24px;'>Aqui veremos un grafico con los datos historicos de indice bursatil S&P 500</p>", unsafe_allow_html=True)
-
-st.image("Bolsa_de_ny.jpg", width=300 )
+image = Image.open("Bolsa_de_ny.jpg")
+st.image(image, caption="Bolsa_de_by.jpg",use_column_width=True )
 datos = pd.read_csv("Datos_históricos_del_S&P_500.csv")
 
 st.bar_chart(datos)
