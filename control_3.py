105788b9900812eac6bd5ce2b404063bac51ac12
Gustavo-7423 <gustavoantonio2310@gmail.com>
2024-11-05 11:57:20 -0300

    Update control_3.py

--- control_3.py
+++ control_3.py
@@ -1,37 +1,17 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
-# Configuración de la barra lateral
-st.sidebar.title("Análisis de Datos del S&P 500")
-st.sidebar.header("Opciones de visualización")
 
-# Cargar archivo CSV
-uploaded_file = st.file_uploader("Sube el archivo de datos históricos del S&P 500", type=["csv"])
-if uploaded_file is not None:
-    # Leer el archivo CSV
-    df = pd.read_csv(uploaded_file, sep=';')
-    
-    # Convertir la columna 'fecha' a tipo datetime
-    df['fecha'] = pd.to_datetime(df['fecha'], format='%Y-%m-%d')
-    
-    # Mostrar los datos del archivo CSV
-    st.write("Datos del archivo CSV:")
-    st.dataframe(df)
+st.sidebar.title("Esta es una prueba")
+st.sidebar.header("Hola esto es una barra lateral")
+st.sidebar.write("Barra lateral ")
 
-    # Selección de columnas para el gráfico
-    st.write("Seleccione las columnas para el gráfico de precios:")
-    x_column = "fecha"  # Columna de fechas para el eje X
-    y_column = st.selectbox("Seleccione el valor a graficar", ["ultimo", "apertura"])
+st.sidebar.image("madrir.png")
 
-    # Crear el gráfico si ambas columnas están seleccionadas
-    if x_column and y_column:
-        fig, ax = plt.subplots()
-        ax.plot(df[x_column], df[y_column], label=y_column, color="blue")
-        ax.set_title(f"Evolución de {y_column.capitalize()} a lo largo del tiempo")
-        ax.set_xlabel("Fecha")
-        ax.set_ylabel(y_column.capitalize())
-        ax.legend()
-
-        # Mostrar el gráfico en Streamlit
-        st.pyplot(fig)
+if st.sidebar.button("Clik en la barra lateral "):
+    st.sidebar.write("Hice un boton lateral ")
+user_input = st.sidebar.text_input("escribe algo en la barra ") # esto hace que el usuario ingrese palabra y aparezcan en la barra lateral 
+st.sidebar.write("Escribiste en la barra:  ", user_input)
+st.title("Esta es mi primera pagina")
+st.header("MI primera página")
+st.image("madrir.png")
