e4e29c09be3acc4322bd71d4cf87d4489018e222
Gustavo-7423 <gustavoantonio2310@gmail.com>
2024-11-05 12:56:04 -0300

    Update control_3.py

--- control_3.py
+++ control_3.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
 import pandas as pd
-
+st.markdown("<p style='font-size:12px;'>Texto pequeño en Streamlit</p>", unsafe_allow_html=True)
 st.markdown("# Datos historicos del indice bursatil S&P_500") 
 st.header("Aqui vemos los datos historicos del indice bursatil s&p 500")
 datos = pd.read_csv("Datos_históricos_del_S&P_500.csv")
