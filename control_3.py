75410ec4489be7bebcf3a584a1a148b23fef1c51
Gustavo-7423 <gustavoantonio2310@gmail.com>
2024-11-05 12:15:27 -0300

    Update control_3.py

--- control_3.py
+++ control_3.py
@@ -4,6 +4,6 @@ import pandas as pd
 
 st.markdown("# Datos historicos del indice bursatil S&P_500") 
 
-datos = pd.read_csv("Datos_históricos_del_S&P_500.csv")
+datos = pd.read_csv("Datos_históricos_del_S&P_500.csv", sep";")
 print(datos)
 st.bar_chart(datos)
